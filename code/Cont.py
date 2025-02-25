d77ba34b322d3ea6e3c4b96eabfb7bad77883b06
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2025-02-25 13:02:31 -0300

    Creating the menu: background, sound and fonts Creating the const file

---
+++ code/Cont.py
@@ -0,0 +1,16 @@
+from pygame.examples.grid import WINDOW_WIDTH
+# W
+WIN_WIDTH=576
+WIN_HEIGHT=324
+
+#C
+COLOR_ORANGE=(255, 128, 0)
+COLOR_WHITE=(255, 255, 255)
+
+#M
+MENU_OPTION=('NEW GAME 1P',
+             'NEW GAME 2P - COOPERATIVE',
+             'NEW GAME 2p - COMPETITIVE',
+             'SCORE',
+             'EXIT'
+             )
