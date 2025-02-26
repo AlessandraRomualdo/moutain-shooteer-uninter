630903142738643ad17a5c8b309d7b15125c3807
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2025-02-26 17:56:56 -0300

    Creating player shot Creating enemy shot

--- code/Level.py
+++ code/Level.py
@@ -7,9 +7,11 @@ from pygame import Surface, Rect
 from pygame.font import Font
 
 from code.Cont import COLOR_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAW_TIME
+from code.Enemy import Enemy
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
 from code.EntityMediator import EntityMediator
+from code.Player import Player
 
 
 class Level:
@@ -35,6 +37,10 @@ class Level:
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
+                if isinstance(ent, (Player, Enemy)):
+                    shoot = ent.shoot()
+                    if shoot is not None:
+                        self.entity_list.append(shoot)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -55,7 +61,6 @@ class Level:
             # verifica a vida e destroi a entidade
             EntityMediator.verify_health(entity_list=self.entity_list)
 
-            pass
 
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
